628ae67710107deb72dbd7b91aa1bcbf5ccc5c64
Jan Dorsman <jan@treehouse.nl>
2016-01-18 17:28:32 +0100

    Updating to Whoops 2.0.0, import Whoops classes in handler

--- src/Error/WhoopsErrorHandler.php
+++ src/Error/WhoopsErrorHandler.php
@@ -3,18 +3,20 @@ namespace WhoopsCakephp\Error;
 
 use Cake\Core\Configure;
 use Cake\Error\BaseErrorHandler;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
 
 class WhoopsErrorHandler extends BaseErrorHandler {
 
 	protected function _displayError($error, $debug) {
-		$Whoops = new \Whoops\Run();
-		$Whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+		$Whoops = new Run();
+		$Whoops->pushHandler(new PrettyPageHandler());
 		return $Whoops->handleError(Configure::read('Error.errorLevel'), $error);
 	}
 
 	protected function _displayException($exception) {
-		$Whoops = new \Whoops\Run();
-		$Whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+		$Whoops = new Run();
+		$Whoops->pushHandler(new PrettyPageHandler());
 		return $Whoops->handleException($exception);
 	}
 
